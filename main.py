03c1913c2c2344aeb829e9d98a844e7884dcc654
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-01-27 12:31:26 +0500

    Create "registration_markup" and add in Commads[start]
    Add bot polling

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import telebot
 from telebot import types
 
-TOKEN = ""
+TOKEN = "855474923:AAES5V4roQy8REHoKJthcsZtOGMf2GbQS98"
 bot = telebot.TeleBot(TOKEN)
-
-keybard = types.ReplyKeyboardMarkup()
-first_item =
 
 @bot.message_handler(commands = ["start"])
 def start(message):
-    bot.send_message(message.chat_id, "Привет, я твойй помошник в бизнесе \nЯ помогу в коммуникации с твоей командой \nВот мои умения:\n-\n-\n- ")
+    registration_markup = types.InlineKeyboardMarkup()
+    registration_markup.add(types.InlineKeyboardButton(text ="Зарегестрироваться", callback_data="registration"))
+    bot.send_message(message.from_user.id, "Привет, я твой помошник в бизнесе \nЯ помогу в коммуникации с твоей командой \nВот мои умения:\n-\n-\n- ", reply_markup=markup)
 
 @bot.message_handler(content_types = ["text"])
+def text_handler(message):
+    bot.send_message(message.from_user.id, "Пожалуйста работайте с меню.")
+
+bot.polling(True)
