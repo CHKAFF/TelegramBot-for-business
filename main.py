060fe3c9aa3cdb64bf5f820c724df046a6b0cb18
CHKAFF <53139664+CHKAFF@users.noreply.github.com>
2020-01-28 20:45:17 +0500

    Add Admin_mode(command) password check

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def start(message):
     registration_markup.add(types.InlineKeyboardButton(text ="Зарегестрироваться", callback_data="registration"))
     bot.send_message(message.from_user.id, "Привет, я твой помошник в бизнесе \nЯ помогу в коммуникации с твоей командой \nВот мои умения:\n-\n-\n- ", reply_markup=registration_markup)
 
+@bot.message_handler(commands = ["admin"])
+def admin(message):
+    if message.text == "/admin adminsuper":
+        bot.send_message(message.from_user.id, "Всё ок")
+    else:
+        bot.send_message(message.from_user.id, "Пароль неверный")
+
 @bot.message_handler(content_types = ["text"])
 def text_handler(message):
     bot.send_message(message.from_user.id, "Пожалуйста работайте с меню.")
@@ -27,7 +34,7 @@ def callback_handler(call):
             if call.data == "registration":
                 bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="Поздравляем, вы стали членом нашей дружно команды!\nТеперь вы будете получать уведомления о совещаниях и предстоящих событиях, так же у вас есть возможность самим предупреждать людей и собирать Фидбэки по прошедшим мероприятиям.", reply_markup=menu_markup)
             elif call.data == "feedback":
-                 bot.send_message(call.from_user.id, )
+                bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id, text="good", reply_markup=menu_markup)
             elif call.data == "event":
                 pass
             elif call.data == "meeting":
